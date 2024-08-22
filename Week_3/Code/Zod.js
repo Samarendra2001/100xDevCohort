61c4cb35df4ff37081347fe7941d7cf7be198782
Samrendra Barick <baricksamarendra@gmail.com>
2024-08-23 01:38:57 +0530

    Installed Zod

---
+++ Week_3/Code/Zod.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const app = express();
+const zod = require("zod")
+
+const schema = zod.array(zod.number());
+
+app.use(express.json());
+
+
+app.post("/health-check-up",((req,res)=>{
+    const kidneys = req.body.kidneys;
+    const response = schema.safeParse(kidneys);
+    res.send({
+        response
+    })
+}))
+
+app.listen(3000);
