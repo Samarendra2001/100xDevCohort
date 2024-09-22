30f0600f8070973c3f1245f38069241b35ff3e13
Samrendra Barick <baricksamarendra@gmail.com>
2024-09-22 18:01:39 +0530

    updated

--- Week_3/Code/Zod2.js
+++ Week_3/Code/Zod2.js
@@ -7,9 +7,10 @@ function ValidateINput(obj){
         email:zod.string().email(),
         password:zod.string().min(6),
     })
+    const response = schema.safeparse(obj);
+    console.log(response);
 }
-const response = schema.safeparse(obj);
-console.log(response);
+
 
 app.post("/login",((req,res)=>{
     const response = ValidateINput(req.body);
@@ -20,5 +21,15 @@ app.post("/login",((req,res)=>{
         return;
     }
 }))
+
+// //In Zod, safeParse is used to validate data while providing a detailed response on 
+//whether the validation was successful or not, without throwing an error. 
+//It returns an object containing two properties: success and data/error.
+
+// Here's a breakdown:
+
+// success: A boolean indicating whether the validation passed (true) or failed (false).
+// data: Contains the validated data if success is true.
+// error: Contains the validation errors if success is false.
 
 app.listen(3000);
