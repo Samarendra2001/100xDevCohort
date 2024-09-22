37a4c6cabb5aee98c746178b1f796f719871cb4e
Samrendra Barick <baricksamarendra@gmail.com>
2024-09-22 18:04:51 +0530

    Frontend Created

---
+++ Week_5/Code/TODoApp/frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import CreateTodo from '../components/CreateTodo'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div><CreateTodo/></div>
+  )
+}
+
+export default App
