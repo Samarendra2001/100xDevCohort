70ef2a38b7f0367e916adfa7d428e17d67362f92
Samrendra Barick <baricksamarendra@gmail.com>
2024-09-25 09:59:54 +0530

    updated atomFamily

--- Week7/code/atom-family/src/App.jsx
+++ Week7/code/atom-family/src/App.jsx
@@ -1,23 +1,45 @@
 import './App.css'
-import { RecoilRoot, useRecoilState } from 'recoil';
+import { RecoilRoot, useSetRecoilState,useRecoilValue } from 'recoil';
 import { todosAtomFamily } from './atoms';
+import { useEffect } from 'react';
+// import { todosAtomFamily } from './atoms';
 
 function App() {
   return <RecoilRoot>
+    <UpdaterComponent />
     <Todo id={1}/>
+    <Todo id={2} />
+    <Todo id={2} />
+    <Todo id={2} />
     <Todo id={2} />
   </RecoilRoot>
 }
 
+
+function UpdaterComponent(){
+  const updateTodo = useSetRecoilState(todosAtomFamily(2));
+  useEffect(()=>{
+    setTimeout(()=>{
+      updateTodo({
+        id:"2",
+        title:"NewTodo",
+        description:"NewDescription"
+      })
+    },5000)
+  },[])
+  return <div></div>
+}
 function Todo({id}) {
-   const [todo, setTodo] = useRecoilState(todosAtomFamily(id));
+   const currentTodo = useRecoilValue(todosAtomFamily(id));
 
   return (
-    <>
-      {todo.title}
-      {todo.description}
-      <br />
-    </>
+    <div>
+      {currentTodo.title} <br />
+      {currentTodo.description}
+    </div>
+      
+      
+    
   )
 }
 
--- Week7/code/atom-family/src/atoms.jsx
+++ Week7/code/atom-family/src/atoms.jsx
@@ -1,9 +1,11 @@
 import { atomFamily } from "recoil";
 import { TODOS } from "./todos";
 
+
+//console.log("Creating todosAtomFamily");
 export const todosAtomFamily = atomFamily({
-  key: 'todosAtomFamily',
-  default: id => {
+  key: 'uniqueTodosAtomfaily',
+  default: (id) => {
     return TODOS.find(x => x.id === id)
   },
 });
